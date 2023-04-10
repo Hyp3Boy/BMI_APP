8a0a1392df9c37fa76af42012b5dc74fc42efddc
Lenin Chavez <lenin.chavez@utec.edu.pe>
2023-04-09 22:03:36 -0500

    Add error alert

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ function bmi() {
   let height = document.getElementById("height").value / 100.0;
 
   let output = Math.round((mass / Math.pow(height, 2)) * 100) / 100;
+  //if the output is NaN it should display an error message
+  if (isNaN(output)) {
+    return window.alert("Please enter a valid number");
+  }
   console.log(output);
   //write the output on the page
   document.getElementById("result").innerHTML = "Your BMI is: " + output;

2ca6842283acd5ff39f38c9aa74fbfb5bfa770a9
Lucas <46696548+slamgLuke@users.noreply.github.com>
2023-04-09 20:51:22 -0500

    /100

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ console.log('Hola mundo');
 
 function bmi() {
   var mass = document.getElementById("mass").value;
-  var height = document.getElementById("height").value * 100;
+  var height = document.getElementById("height").value / 100.0;
   
   var output = mass / Math.pow(height, 2);
   console.log(output);
